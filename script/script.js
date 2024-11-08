d6595ab7a200d767d0570d0cb2107f4e99f2c6b5
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-08 15:34:46 +0100

    generazione biglietto al submit con js (incompleto)

--- script/script.js
+++ script/script.js
@@ -31,8 +31,57 @@ function handleSubmit(e) {
     // variabile per il risultato
     const finalPrice = calcPrice(km.value, eta.value);
     // inserisco come innerText di un elemento il valore di finalPrice
-    priceText.innerText = finalPrice.toFixed(2) + "€";
+    ticketForm.insertAdjacentHTML("afterend", 
+        `
+        <div class="border border-1 border-dark rounded-3 p-3">
+            <div class="d-flex justify-content-between align-items-center">
+                <h2>${finalPrice.toFixed(2) + "€"}</h2>
+                <div>
+                    <h3 class="text-uppercase">codice biglietto</h3>
+                    <h2 id="ticket-code" class="text-uppercase">alphanumeric</h2>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between">
+                <div class="d-flex flex-column">
+                    <span>PARTENZA</span>
+                    <span>MILANO CENT.</span>
+                    <span>00:00</span>
+                </div>
+                <div class="d-flex flex-column">
+                    <span>ARRIVO</span>
+                    <span>ROMA TIB.</span>
+                    <span>00:00</span>
+                </div>
+                <div class="d-flex flex-column">
+                    <span>DATA PARTENZA</span>
+                    <span id="ticket-date"></span>
+                </div>
+                <div class="d-flex flex-column">
+                    <span>TRENO</span>
+                    <span id="train-number"></span>
+                </div>
+            </div>
+            <table class="border border-1 border-danger w-100">
+                <tr>
+                    <th class="w-25">NOME PASSEGGERO</th>
+                    <th>TIPO</th>
+                    <th>EXTRA</th>
+                    <th>CARROZZA</th>
+                    <th>POSTO</th>
+                </tr>
+                <tr>
+                    <td>${fname.value} ${lname.value}</td>
+                    <td>${eta.options[eta.selectedIndex].innerHTML}</td>
+                    <td id="carriage-number"></td>
+                    <td id="seat-number"></td>
+
+                </tr>
+            </table>
+        </div>
+        `
+    )
 } 
+console.log();
 
 //! functions
 /**
